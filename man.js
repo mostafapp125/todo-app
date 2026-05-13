4a0a7aa9f8f60a63db321faabd88a84128083aa3
mostafapp125 <godamostafa717@gmail.com>
2026-05-13 13:40:34 +0300

    first commit

---
+++ man.js
@@ -0,0 +1,21 @@
+// // textContent // يجلب كل النص حتى المخفي
+// // innerText   // يجلب النص الظاهر فقط
+// // innerHTML   // يجلب HTML كامل
+// let div = document.querySelector("div")
+
+// function stop(){
+// div.innerHTML -= 1
+// if(div.innerHTML === "0"){
+//  clearInterval(count)
+// }
+// }
+// let count = setInterval(stop,1)
+
+// // let a = document.querySelector("div")
+// // function stop(){
+// //     a.innerHTML -= 1 
+// //     if(a.innerHTML === "0"){
+// //         clearInterval(sto)
+// //     }
+// // } 
+// // let sto = setInterval(stop, 1000)
